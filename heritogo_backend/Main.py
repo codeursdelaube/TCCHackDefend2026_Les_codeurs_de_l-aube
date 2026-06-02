388fbf739349eeee4abdf33aeabc523358e8011f
samuelafanou4-source <samuelafanou4@gmail.com>
2026-06-02 14:45:21 +0000

    chargement de Pydantic Settings avec le fichier .env

--- heritogo_backend/Main.py
+++ heritogo_backend/Main.py
@@ -11,3 +11,23 @@ from haversine import calcul_de_l_haversine
 from fastapi import Security, Depends
 from fastapi.security import api_key
 
+
+# ==========================================
+# 1. CONFIGURATION ET VARIABLES D'ENVIRONNEMENT
+# ==========================================
+
+class Settings(BaseSettings):
+    """
+    Gestion centralisée des variables de configuration avec   Pydantic Settings.
+    Charge automatiquement les variables stockées dans le fichier '.env'.
+    """
+    gemini_api_key: str # Clé secrète pour s'authentifier auprès de l'API Google Gemini
+    api_secret_key: str # Clé secrète requise pour sécuriser l'accès à certaines routes de notre API
+    
+    # Configuration pour lier Pydantic au fichier physique .env
+    model_config = SettingsConfigDict(env_file=".env")
+
+    # Instanciation des paramètres pour une utilisation globale
+settings = Settings()
+
+
